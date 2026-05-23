86974ce31e90f6848c536ad27743aa3e623fce5a
999mstfatema-rgb <999mstfatema@gmail.com>
2026-05-23 15:31:46 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdbHoaDhKjlydvDL1HDu9KuVSBEeGTkIA",
+  authDomain: "farhans-diary.firebaseapp.com",
+  projectId: "farhans-diary",
+  storageBucket: "farhans-diary.firebasestorage.app",
+  messagingSenderId: "856421576567",
+  appId: "1:856421576567:web:9fe122351bf06902defb72",
+  measurementId: "G-5MSXBKMQ63"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
